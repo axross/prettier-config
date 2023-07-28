e7a736199939a5fe3e143863ca28be2f84b30af7
Kohei Asai <yo@kohei.dev>
2023-07-28 00:15:34 -0700

    feat: remove tailwind plugin

--- prettier.config.js
+++ prettier.config.js
@@ -2,7 +2,6 @@
 
 module.exports = {
   plugins: [
-    require("prettier-plugin-tailwindcss"),
     require("@trivago/prettier-plugin-sort-imports"),
   ],
   importOrder: ["server-only", "<THIRD_PARTY_MODULES>", "^~", "^[./]", ".css$"],
