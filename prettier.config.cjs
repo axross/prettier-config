04b48965ec77fb2ce0057d997f783f74d2d9fa29
Kohei Asai <yo@kohei.dev>
2024-11-23 11:26:35 -0800

    feat: update supporting prettier version and optional plugins

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,14 +1,23 @@
 /* eslint-env node */
-const sortImportPlugin = require("@trivago/prettier-plugin-sort-imports");
 
-module.exports = {
-  plugins: [sortImportPlugin],
-  importOrder: [
+/** @type {import("prettier").Config} */
+const config = {
+  plugins: [],
+};
+
+if (hasPkg("@trivago/prettier-plugin-sort-imports")) {
+  const sortImportPlugin = require("@trivago/prettier-plugin-sort-imports");
+
+  config.plugins.push(sortImportPlugin);
+
+  config.importOrder = [
     "server-only",
     "^node:",
     "<THIRD_PARTY_MODULES>",
     "^~",
     "^[./]",
     ".css$",
-  ],
-};
+  ];
+}
+
+module.exports = config;
